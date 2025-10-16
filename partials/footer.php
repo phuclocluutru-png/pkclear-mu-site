2dd864ab17d1ccf6d66b81fe43f4b49bdbd506d6
phuclocluutru-png <phuclocluutru@gmail.com>
2025-10-16 18:49:49 +0700

    Delete footer.php

--- partials/footer.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-$year = (int) date('Y');
-?>
-<footer class="border-t border-white/5 py-8 text-center text-sm text-slate-400">
-  © <?= $year; ?> PK CLEAR. All rights reserved.
-</footer>
